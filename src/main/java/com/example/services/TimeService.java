4738d0d140dfe8f1690d3ff246675292f164310a
mlecoutre <mlecoutre@gmail.com>
2012-05-03 17:46:08 +0200

    add timezone

--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -1,20 +1,24 @@
 package com.example.services;
 
 import com.example.models.Time;
-
+import java.util.TimeZone;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 @Path("/time")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces("application/json;charset=UTF-8")
 public class TimeService {
 
-    @GET
-    public Time get() {
-        return new Time();
-    }
+	@GET
+	public Time get() {
+		return new Time();
+	}
 
+	@GET
+	@Path("/{timezone}")
+	public Time get(@PathParam("timezone") String timezone) {
+		return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
+	}
 }
-
