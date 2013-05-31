7a184fc63c8bdaa184b905d8e8b01a976d2b2bf5
mlecoutre <mlecoutre@gmail.com>
2013-05-31 22:24:31 +0200

    add update part on run screen

--- src/main/webapp/ang/js/run.js
+++ src/main/webapp/ang/js/run.js
@@ -4,27 +4,34 @@ function RunCtrl($scope, User, Appointment, Children, Nurse, AppService) {
     $scope.accountId = 1;
 
     $scope.children = listChildren();
-    $scope.users = listUsers();
-    $scope.last5 = [];
-    $scope.nurses = listNurses();
-    $kids =[];
+    $scope.users    = listUsers();
+    $scope.last5    = [];
+    $scope.nurses   = listNurses();
+    $scope.kids;
 
     $scope.requestAppointments;
 
     $scope.appointment = Appointment.get({appointmentId:0, filter: 'account', value: $scope.accountId, filter2: 'user', value2:1});
     $scope.appointments;
+    $scope.updtAppointement;
+
+    $scope.searchDate = "currentWeek";
 
      initAppointment();
     //MANAGE Appointments ///////////
     function initAppointment(){
        console.log("init");
        last5Appointments();
-
-
     }
 
     $scope.addAppointment = function(){
         console.log("add Appointment: ");
+        var kidsArr = new Array();
+        angular.forEach($scope.kids, function(kid) {
+                console.log (" add kid "+kid.childId);
+                kidsArr.push(kid.childId);
+        });
+        $scope.appointment.kidIds =  kidsArr;
         Appointment.save($scope.appointment, function(a) {
             console.log("add Appointment created "+a.accountId);
             $scope.appointment = new Appointment();
@@ -36,16 +43,41 @@ function RunCtrl($scope, User, Appointment, Children, Nurse, AppService) {
     }
 
     $scope.clicKid =  function(child){
-        console.log(">>"+child.childId);
-        $scope.kids.push(child.childId);
+        var isToAdd = true;
+        var index = 0;
+        angular.forEach($scope.kids, function(kid) {
+              if (kid.childId == child.childId){
+                //remove it
+                isToAdd = false;
+                var value = $scope.kids.splice( index, 1 );
+                console.log("Remove child ");
+                return;
+              }
+              index++;
+        });
+        if (isToAdd){
+            console.log("Add child");
+            $scope.kids.push(child);
+        }
+
     }
 
     $scope.prepareUpdateAppointment = function(a){
          console.log("prepareUpdate ");
-         $scope.appointment = a;
-         $("#updateAppointment").show();
-         $("#cancelAppointment").show();
-         $("#addAppointment").hide();
+         $scope.updtAppointement = a;
+          angular.forEach($scope.users, function(u) {
+            if(u.userId == a.arrivalUser.userId ) {
+                $scope.updtAppointement.arrivalUser = u;
+            }
+         });
+         angular.forEach($scope.users, function(u) {
+            if(u.userId == a.departureUser.userId ) {
+                 $scope.updtAppointement.departureUser = u;
+            }
+         });
+         $("#editUpdate").show();
+         //$("#cancelAppointment").show();
+         //$("#addAppointment").hide();
          return;
     }
 
@@ -71,9 +103,9 @@ function RunCtrl($scope, User, Appointment, Children, Nurse, AppService) {
 
     $scope.cancelAppointment = function(){
        $scope.appointment = new Appointment();
-       $("#updateAppointment").hide();
-       $("#addAppointment").show();
-       $("#cancelAppointment").hide();
+       $("#editUpdate").hide();
+      // $("#addAppointment").show();
+      // $("#cancelAppointment").hide();
     }
 
     function last5Appointments(){
@@ -82,14 +114,20 @@ function RunCtrl($scope, User, Appointment, Children, Nurse, AppService) {
         });
     }
 
-    function listAppointments() {
-           return $scope.last5 = Appointment.query({appointmentId:0, filter: 'account', value: $scope.accountId, filter2:'searchType', value2: 'last'});
-        };
+    $scope.listAppointments =  function() {
+        console.log("listAppointments : "+$scope.searchDate);
+        Appointment.report({appointmentId:0, filter: 'account', value: $scope.accountId, filter2:'searchType', value2: $scope.searchDate}, function(u, getResponseHeaders){
+            $scope.requestAppointments = u.appointments;
+        });
+    };
 
     //////// OTHER STUFFS ////////////////////
 
     function listChildren() {
-          return $scope.children  = Children.query({childId:0, filter: 'account', value: $scope.accountId});
+           return $scope.children  = Children.query({childId:0, filter: 'account', value: $scope.accountId}, function(){
+                //clone the kids list in order to init the selected list of current appointment
+                $scope.kids = $scope.children.slice(0);
+           });
     };
 
     function listNurses() {
